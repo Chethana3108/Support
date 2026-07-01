06b68b3178dcd6e3d655cb7be1aacc86db6077ca
Chethana3108 <chethana@biztechnosys.com>
2026-07-01 17:36:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, access_log=False)
+    uvicorn.run("main:app", host="145.223.22.6", port=8000, reload=True, access_log=False)
